659d2d08fa883c20e9352de1e441c78232f8f302
Tomasz Krasieńko <tomasz.krasienko@outlook.com>
2023-07-26 16:55:07 +0200

    Added solution with canellation token

--- CancellationTokenCheck/ConsoleApplication/Program.cs
+++ CancellationTokenCheck/ConsoleApplication/Program.cs
@@ -1,39 +1,58 @@
 ﻿using System.Diagnostics;
 
+var source = new CancellationTokenSource();
+var token = source.Token;
+//CancellationToken needs to be make from CancellationTokenSource to have all functionality 
+
+source.CancelAfter(TimeSpan.FromSeconds(2));
+
 var sw = new Stopwatch();
 sw.Start();
 var tasks = new[]
 {
-    new Test().DoIterativeJobAsync(),
-    new Test().DoHttpJobAsync()
+    new Test().DoIterativeJobAsync(token),
+    new Test().DoHttpJobAsync(token)
 };
 
-await Task.WhenAll(tasks);
-
+try
+{
+    await Task.WhenAll(tasks);
+}
+catch(OperationCanceledException ex) when (ex.CancellationToken == token)
+{
+    Console.WriteLine("Operation has been cancelled. Exception thrown");
+}
+finally
+{
+    source.Dispose();
+}
 
 sw.Stop();
 Console.WriteLine($"Operation cancelled within {sw.Elapsed.TotalMilliseconds} ms.");
-//Without cancellation token result -> Operation cancelled within 10074,6948 ms.
+// Results with cancellation token ->
+// Operation has been cancelled. Exception thrown
+// Operation cancelled within 2013,4343 ms.
+// Result near to 2 sec because of line 7: source.CancelAfter(TimeSpan.FromSeconds(2));
 
 
 class Test
 {
-    public async Task DoIterativeJobAsync()
+    public async Task DoIterativeJobAsync(CancellationToken token)
     {
         var i = 0;
         while(i < 30)
         {
-            await Task.Delay(100);
+            await Task.Delay(100, token);
             Console.WriteLine($"Do iterative job async no: {i}");
             i++;
         }
     }
 
-    public async Task DoHttpJobAsync()
+    public async Task DoHttpJobAsync(CancellationToken token)
     {
         var httpClient = new HttpClient();
         //Only for testing reasons. For production solutions better use httpClientFactory
 
-        await httpClient.GetAsync("http://localhost:5043");
+        await httpClient.GetAsync("http://localhost:5043", token);
     }
 }
