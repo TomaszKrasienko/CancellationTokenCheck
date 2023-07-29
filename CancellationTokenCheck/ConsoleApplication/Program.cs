b0d44f50fcd0e5ca68cb60d278d1d28af94463ff
Tomasz Krasieńko <tomasz.krasienko@outlook.com>
2023-07-29 12:14:42 +0200

    Added full CT handling

--- CancellationTokenCheck/ConsoleApplication/Program.cs
+++ CancellationTokenCheck/ConsoleApplication/Program.cs
@@ -2,6 +2,8 @@
 
 var source = new CancellationTokenSource();
 var token = source.Token;
+var token2 = source.Token;
+//CancellationTokenSource.CreateLinkedTokenSource(token, token2).CancelAfter(TimeSpan.FromSeconds(1));
 //CancellationToken needs to be make from CancellationTokenSource to have all functionality 
 
 source.CancelAfter(TimeSpan.FromSeconds(2));
@@ -10,7 +12,7 @@ var sw = new Stopwatch();
 sw.Start();
 var tasks = new[]
 {
-    new Test().DoIterativeJobAsync(token),
+    new Test().DoIterativeJobAsync(token2),
     new Test().DoHttpJobAsync(token)
 };
 
@@ -22,6 +24,7 @@ catch(OperationCanceledException ex) when (ex.CancellationToken == token)
 {
     Console.WriteLine("Operation has been cancelled. Exception thrown");
 }
+
 finally
 {
     source.Dispose();
@@ -40,9 +43,13 @@ class Test
     public async Task DoIterativeJobAsync(CancellationToken token)
     {
         var i = 0;
-        while(i < 30)
-        {
+        while(i < 30 && token.IsCancellationRequested == false)
+        { 
+            //Do something
             await Task.Delay(100, token);
+            //If cancellationToken will be only in while, returned Task from method will have RunToCompletion status
+            //because token won't have info on Task level
+            //In while this should be only optional
             Console.WriteLine($"Do iterative job async no: {i}");
             i++;
         }
@@ -53,6 +60,14 @@ class Test
         var httpClient = new HttpClient();
         //Only for testing reasons. For production solutions better use httpClientFactory
 
-        await httpClient.GetAsync("http://localhost:5043", token);
+        //Usage of callback 
+        token.Register(() =>
+        {
+            Console.WriteLine("Cancelling HTTP request ...");
+            httpClient.CancelPendingRequests();
+        });
+        await httpClient.GetAsync("http://localhost:5043");
+        
+        //await httpClient.GetAsync("http://localhost:5043", token);
     }
 }
--- CancellationTokenCheck/WebApplication/Program.cs
+++ CancellationTokenCheck/WebApplication/Program.cs
@@ -4,7 +4,7 @@
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHostedService<Job>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,11 +17,35 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.MapGet("/", async (HttpContext ctx ) =>
-{
-    await Task.Delay(10_000);
-    Console.WriteLine("Ok");
-})
-.WithOpenApi();
+app.MapGet("/", async (HttpContext ctx, CancellationToken token) => 
+    {
+        token.Register(() =>
+        {
+            Console.WriteLine("HTTP request aborted ...");
+        });
+        try
+        {
+            await Task.Delay(10_000, token);
+        }
+        catch (OperationCanceledException ex)
+        {
+            Console.WriteLine("Operation cancelled");
+            return;
+        }
+        Console.WriteLine("Ok");
+    }).WithOpenApi();
 
 app.Run();
+
+class Job : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (stoppingToken.IsCancellationRequested is false)
+        {
+            await Task.Delay(1_000);
+        }
+        //To stop press ctrl + c
+        Console.WriteLine("Application is closing ... ");
+    }
+}
